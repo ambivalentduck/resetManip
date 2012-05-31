4c09176e21225048348f933267eb6e90a1731367
Justin Horowitz <justin.horowitz@gmail.com>
2012-05-31 12:52:53 -0500

    Included plots designed at work

--- Inverse dynamics/processCount.m
+++ Inverse dynamics/processCount.m
@@ -20,17 +20,44 @@ lgains=log(gains);
 
 dm=[data.metrics];
 counts=[dm.count];
+mcount=max(counts);
 dlgains=[data.lgains];
 
-[a,b,c]=unique(dlgains);
 figure(1)
 clf
+dnm=[datanull.metrics];
+nullcount=[dnm.count];
+h0=hist(nullcount,1:mcount);
+h0=h0./sum(h0);
+bar(1:mcount,h0)
+xlabel('Submovements')
+ylabel('Relative Frequency')
+title('Histogram of Submovement Count - No Force Probe Trials')
 
+[a,b,c]=unique(dlgains);
+figure(2)
+clf
 for k=1:6
     subplot(6,1,k)
-    hist(counts(c==k))
+    h=hist(counts(c==k),1:mcount);
+    bar(1:mcount,h./sum(h))
+    ylabel(num2str(exp(a(k))))
+
+end
+xlabel('Submovements')
+suplabel('Gain','y');
+suplabel('Histograms of Submovement Count - Force Probe Trials','t');
+
+figure(3)
+clf
+for k=1:6
+    subplot(6,1,k)
+    h=hist(counts(c==k),1:mcount);
+    bar(1:mcount,h./sum(h)-h0)
     ylabel(num2str(exp(a(k))))
 end
 xlabel('Submovements')
-suplabel('Gain','y')
-suplabel('Histogram of Submovement Count','t')
+suplabel('Gain','y');
+suplabel('Histograms of Submovement Count - Comparison','t');
+
+
