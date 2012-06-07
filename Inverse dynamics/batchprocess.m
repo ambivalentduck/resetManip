0b83c209ef2c95308baec45f54dc1ef7cd269b54
Justin Horowitz <justin.horowitz@gmail.com>
2012-06-07 14:11:10 -0500

    Added processing of the entire 8-subject data set.

---
+++ Inverse dynamics/batchprocess.m
@@ -0,0 +1,57 @@
+for k=1:8
+    ks=num2str(k)
+    try
+        subcells{k}=load(['./Data/',ks,'rta.mat']);
+    catch
+        try
+            processCount(ks,10*k);
+        catch
+            addSubjectPlus(ks);
+            makeParagons(ks);
+            processCount(ks,10*k);
+        end
+    end
+end
+
+time=-10*20:10:10*20;
+gains=[.8 1 2 4 8 16];
+subaves=zeros(length(subcells),length(gains),length(time));
+for k=1:length(subcells)
+    figure(k)
+    clf
+
+    for g=1:length(gains)
+        subplot(length(gains),1,g)
+        hold on
+
+        for kk=1:length(time)
+            subaves(k,g,kk)=mean(subcells{k}.distCell{g,kk});
+            semilogy(time(kk)*ones(size(subcells{k}.distCell{g,kk})),subcells{k}.distCell{g,kk},'b')
+            %Have to go back to processCount in order to save the original
+            %matrix with the -1s since you can't recover lines otherwise.
+        end
+        semilogy(time,squeeze(subaves(k,g,:)),'r')
+        ylabel(num2str(gains(g)))
+    end
+    xlabel('Time since Reset, ms');
+    suplabel('Kp Gain','y');
+    suplabel(['Reset-Triggered Distance Error: Sub#',num2str(k),', cm'],'t');
+end
+
+figure(length(subcells)+1)
+clf
+for g=1:length(gains)
+    subplot(length(gains),1,g)
+    hold on
+    for k=1:length(subcells)
+        plot(time,squeeze(subaves(k,g,:))/max(subaves(k,g,:)),'Color',[.5 .5 .5])
+    end
+    for k=1:length(time)
+        allMean(g,k)=mean(subaves(:,g,k));
+    end
+    plot(time,allMean(g,:)/max(allMean(g,:)),'r')
+    ylabel(num2str(gains(g)))
+end
+xlabel('Time since Reset, ms');
+suplabel('Kp Gain','y');
+suplabel('Reset-Triggered Distance Error: Across-Subject Comparison and Mean, cm','t');
--- Inverse dynamics/processCount.m
+++ Inverse dynamics/processCount.m
@@ -1,13 +1,15 @@
-clc
-clear all
-name='1';
+function processCount(name, plotoffset)
+
+if nargin<2
+    plotoffset=0;
+end
 
 %% Load Data
 load(['./Data/',name,'.mat']);
 try
     load(['./Data/',name,'humps.mat']);
 catch
-    data=countHumps(name);
+    data=countHumps(name, 0);
     save(['./Data/',name,'humps.mat'],'data')
 end
 
@@ -27,7 +29,7 @@ counts=[dm.count];
 mcount=max(counts);
 dlgains=[data.lgains];
 
-figure(1)
+figure(1+plotoffset)
 clf
 dnm=[datanull.metrics];
 nullcount=[dnm.count];
@@ -39,7 +41,7 @@ ylabel('Relative Frequency')
 title('Histogram of Submovement Count - No Force Probe Trials')
 
 [a,b,c]=unique(dlgains);
-figure(2)
+figure(2+plotoffset)
 clf
 for k=1:length(a)
     subplot(length(a),1,k)
@@ -52,7 +54,7 @@ xlabel('Submovements')
 suplabel('Kp Gain','y');
 suplabel('Histograms of Submovement Count - Force Probe Trials','t');
 
-figure(3)
+figure(3+plotoffset)
 clf
 for k=1:length(a)
     subplot(length(a),1,k)
@@ -73,7 +75,7 @@ for k=1:length(dm)
     filt(k).gain=c(k);
 end
 
-figure(4)
+figure(4+plotoffset)
 clf
 for k=1:length(a)
     subplot(length(a),1,k)
@@ -85,7 +87,7 @@ xlabel('Spatial Frequency');
 suplabel('Kp Gain','y');
 suplabel('Histogram of Submovement Count','t');
 
-figure(5)
+figure(5+plotoffset)
 clf
 for k=1:length(a)
     subplot(length(a),1,k)
@@ -107,27 +109,31 @@ f=find([rawdata.forces]==1);
 for k=1:length(data)
     [data(k).completion,data(k).signedError]=reachOrthoRot(data(k).metrics(1).real(:,1:2)',trials(f(k)).target);
     for g=1:length(a)
-            [data(k).metrics(g).completion,data(k).metrics(g).signedError]=reachOrthoRot(data(k).metrics(g).intendedP,trials(f(k)).target);
-            differenceRhumb=abs(twoNearestNeighbor(data(k).signedError,data(k).completion,data(k).metrics(g).completion)-data(k).metrics(g).signedError);
-            realPnn=twoNearestNeighbor(data(k).metrics(g).real(:,1:2),data(k).metrics(g).realT,data(k).metrics(g).intendedT);
-            differenceDist=sqrt(sum((realPnn-data(k).metrics(g).intendedP').^2,2));
-            
-            speed2=sum((data(k).metrics(g).intendedV).^2); %skipping the square root adds speed
-            [vals,mins]=findpeaks(1./speed2);
-            f2=find(dm((k-1)*length(a)+g).sizes>=.01); %ignore any "reset" less than 1 cm since that's noiseland
-            m=mins(f2);
-            alignedRhumb(g).trial(k).mat=-1*ones(2*span+1,length(m));
-            alignedDist(g).trial(k).mat=-1*ones(2*span+1,length(m));
-            for kk=1:length(m)
-                range=max(1,m(kk)-span):min(length(data(k).metrics(g).intendedT),m(kk)+span);
-                alignedRange=range-m(kk)+1+span;
+        [data(k).metrics(g).completion,data(k).metrics(g).signedError]=reachOrthoRot(data(k).metrics(g).intendedP,trials(f(k)).target);
+        differenceRhumb=abs(twoNearestNeighbor(data(k).signedError,data(k).completion,data(k).metrics(g).completion)-data(k).metrics(g).signedError);
+        realPnn=twoNearestNeighbor(data(k).metrics(g).real(:,1:2),data(k).metrics(g).realT,data(k).metrics(g).intendedT);
+        differenceDist=sqrt(sum((realPnn-data(k).metrics(g).intendedP').^2,2));
+
+        speed2=sum((data(k).metrics(g).intendedV).^2); %skipping the square root adds speed
+        [vals,mins]=findpeaks(1./speed2);
+        f2=find(dm((k-1)*length(a)+g).sizes>=.01); %ignore any "reset" less than 1 cm since that's noiseland
+        m=mins(f2);
+        alignedRhumb(g).trial(k).mat=-1*ones(2*span+1,length(m));
+        alignedDist(g).trial(k).mat=-1*ones(2*span+1,length(m));
+        for kk=1:length(m)
+            range=max(1,m(kk)-span):min(length(data(k).metrics(g).intendedT),m(kk)+span);
+            alignedRange=range-m(kk)+1+span;
+            if sum(abs(imag(differenceRhumb(range))))==0
                 alignedRhumb(g).trial(k).mat(alignedRange,kk)=differenceRhumb(range)';
+            end
+            if sum(abs(imag(differenceDist(range))))==0
                 alignedDist(g).trial(k).mat(alignedRange,kk)=differenceDist(range)';
             end
+        end
     end
 end
 
-figure(6)
+figure(6+plotoffset)
 clf
 time=-10*span:10:10*span;
 rtaRhumb=zeros(length(a),2*span+1);
@@ -137,7 +143,7 @@ for g=1:length(a)
         temp=matRhumb(k,:);
         rtaRhumb(g,k)=mean(temp(temp>=0)); %this filters out the -1s
     end
-    
+
     subplot(length(a),1,g)
     plot(time,rtaRhumb(g,:))
     ylabel(num2str(exp(a(g))))
@@ -146,18 +152,21 @@ xlabel('Time since Reset, ms');
 suplabel('Kp Gain','y');
 suplabel('Reset-Triggered Unsigned Difference: Dist from Rhumb Line, cm','t');
 
-figure(7)
+figure(7+plotoffset)
 clf
 time=-10*span:10:10*span;
 rtaDist=zeros(length(a),2*span+1);
+distCell=cell(length(a),2*span+1);
 for g=1:length(a)
     matDist=[alignedDist(g).trial.mat];
 
     for k=1:2*span+1
         temp=matDist(k,:);
-        rtaDist(g,k)=mean(temp(temp>=0)); %this filters out the -1s
+        vals=temp(temp>=0);
+        rtaDist(g,k)=mean(vals); %this filters out the -1s
+        distCell{g,k}=vals;
     end
-    
+
     subplot(length(a),1,g)
     plot(time,rtaDist(g,:))
     ylabel(num2str(exp(a(g))))
@@ -165,3 +174,5 @@ end
 xlabel('Time since Reset, ms');
 suplabel('Kp Gain','y');
 suplabel('Reset-Triggered Distance: Physical and Commanded, cm','t');
+
+save(['./Data/',name,'rta.mat'],'distCell')
