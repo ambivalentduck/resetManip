ce18ee21c91e1b7d85a730b7607a58fb66d6f552
Justin Horowitz <justin.horowitz@gmail.com>
2012-06-14 14:51:52 -0500

    Highly improved sinusoid fitting

--- Inverse dynamics/fitSBSW.m
+++ Inverse dynamics/fitSBSW.m
@@ -6,8 +6,12 @@ elseif nargin<5
     test=0;
 end
 
+if size(t,1)>size(t,2)
+    t=t';
+end
+
 if test
-    X=zeros(6*test,1);
+    Xr=zeros(6*test,1);
     for k=1:test
         if k==1 %Guarantee a starting point
             t0=t(1);
@@ -22,41 +26,29 @@ if test
         end
         vec=randn(2,1);
         p0=randn(2,1);
-        X(1+6*(k-1):6*k)=[t0;tf;p0;vec];
+        Xr(1+6*(k-1):6*k)=[t0;tf;p0;vec];
     end
-    X
-    [p,v]=SBsinX(t,X);
+    [p,v]=SBsinX(t,Xr);
 end
-
-     
 
 %Step 1: fit the speed profile with sin. support is min-min, peak is height
 %Step 2: Add direction.  Assume peaks are least polluted.
 
 speed=sum(v.^2);
-[vals,mins]=findpeaks(1./speed);
 [vals,maxes]=findpeaks(speed);
-
-%Sanity check, should be one more min than there are maxes. If not, drop
-%the first max.  The only "sane" values here are 0 and -1
-countcheck=length(maxes)-length(mins)+1 %#ok<NOPRT>
-if countcheck<0
-    vals=vals(2:end);
-    maxes=maxes(2:end);
+[trash,mins]=findpeaks(1./speed); %If empty, humps are entirely distinct
+if isempty(mins) %Instead grab the first and last significantly non-zero point around each hump
+    f=find(speed<.05);
+    mins=zeros(2*length(maxes),1);
+    for k=1:length(maxes)
+        mins(2*k-1)=find(f<maxes(k),1,'last');
+        mins(2*k)=find(f<maxes(k),1,'last');
+    end
 end
 
-X=zeros(6*length(maxes),1);
-for k=1:length(maxes)
-    params(k).t0=mins(k);
-    params(k).tf=mins(k+1);
-    M=sqrt(vals(k));
-    dir=v(:,maxes(k))/norm(v(:,maxes(k)));
-    params(k).vec=M*dir/(2*pi/(mins(k)-mins(k+1)));
-    params(k).p0=p(:,maxes(k))-vec;
-    X(1:6)=[params(k).t0;params(k).tf;params(k).p0;params(k).vec];
-end
-    
-%Step 3?: Refine with fminunc
+X=extractHumps(t,p,v,mins,maxes,length(maxes));
+[X Xr]
+% Step 3?: Refine with fminunc
 % options = optimset('GradObj','on');
 % hackyhandle=@(x) SBsinOpt(x,t',p);
 % Xopt=fminunc(hackyhandle,X,options);
@@ -66,50 +58,101 @@ if doplot
     clf
     subplot(2,1,1)
     hold on
-    plot(p(:,1),p(:,2),'b')
-    [f,fp]=SBsinX(t',X);
+    plot(p(1,:),p(2,:),'b')
+    [f,fp,vparts]=SBsinX(t,X);
     speedX=sqrt(sum(fp.^2));
-    plot(f(:,1),f(:,2),'r')
+    plot(f(1,:),f(2,:),'r')
     subplot(2,1,2)
+    hold on
     plot(t, sqrt(speed),'b')
-    plot(t, speedX,'r')
+    plot(t, speedX,'r.')
+    for k=1:length(vparts)
+        plot(t,sqrt(sum(vparts{k}.^2)),'g')
+    end
 end
+params=X;
 
-function [f,fp]=SBsin(t,p0,vec,t0,tf)
+
+function [f,fp]=SBsin(t,pM,vM,t0,tf)
 f=zeros(2,length(t));
-range=find((t<=t0)&(t>=tf));
-f(:,range)=p0*ones(1,length(range))+vec*sin(2*pi*(t(range)-t0)/(tf-t0));
+range=find((t>=t0)&(t<=tf));
+f(:,range)=pM*ones(1,length(range))-((tf-t0)/pi)*vM*cos(pi*(t(range)-t0)/(tf-t0));
 if nargout>1
     fp=zeros(2,length(t));
-    fp(:,range)=2*pi/(tf-t0)*vec*cos(2*pi*(t(range)-t0)/(tf-t0));
+    fp(:,range)=vM*sin(pi*(t(range)-t0)/(tf-t0));
 end
 
-function [p,v]=SBsinX(t,X)
+
+function [p,v,vparts]=SBsinX(t,X)
 p=zeros(2,length(t));
 v=p;
 for k=1:length(X)/6
     [F,FP]=SBsin(t,X((3:4)+6*(k-1)),X((5:6)+6*(k-1)),X(1+6*(k-1)),X(2+6*(k-1)));
     p=p+F;
     v=v+FP;
+    vparts{k}=FP;
 end
+
 
 function [cost,grad]=SBsinOpt(X,t,p)
-%Assume form: X=[t0; tf; p0; vec] Since p0 and vec are each 2x1, 6 elements
-%Remember that for EACH 6 element group, we add another wave.
-%Cost function: .5*sum((fpredicted-freal).^2)
+% Assume form: X=[t0; tf; pM; vM] Since pM and vM are each 2x1, 6 elements
+% Remember that for EACH 6 element group, we add another wave.
+% Cost function: .5*sum((fpredicted-freal).^2)
 L=length(X)/6;
-if nargout>1
-    grad=zeros(size(X));
+grad=X;
+
+P=zeros(2,length(t));
+V=P;
+for k=1:length(X)/6
+    [p_,v_]=SBsin(t,X((3:4)+6*(k-1)),X((5:6)+6*(k-1)),X(1+6*(k-1)),X(2+6*(k-1)));
 end
 
-vals=zeros(2,length(t));
-for k=1:L
-    vals=vals+SBsin(t,X(5+6*(k-1)),X(6+6*(k-1)),X((1:2)+6*(k-1)),X((3:4)+6*(k-1)));
-    grad(5+6*(k-1))=1;
-end
 vmp=vals-p;
 cost=.5*sum(vmp.^2);
 
+
+function X=extractHumps(t,p,v,mins,maxes,N)
+peakv=zeros(N,1);
+X=zeros(6*N,1);
+for k=1:length(maxes)
+    i=mins(mins<maxes(k)); %Find the last min before the kth max
+    if isempty(i)
+        t0=t(1);
+    else
+        t0=t(i(end));
+    end
+
+    i=mins(mins>maxes(k)); %Find the first min after the kth max
+    if isempty(i)
+        tf=t(end);
+    else
+        tf=t(i(1));
+    end
+    %Shift tf and t0 to be symettrically distant from the peak
+    shift=abs((tf-t0)/2);
+    params(k).tf=t(maxes(k))+shift;
+    params(k).t0=t(maxes(k))-shift;
+    
+    params(k).vM=v(:,maxes(k)); %Velocity at peak
+    params(k).pM=p(:,maxes(k)); %Position at peak
+
+    X(1+6*(k-1):6*k)=[params(k).t0;params(k).tf;params(k).pM;params(k).vM];
+    peakv(k)=maxes(k);
+end
+
+for k=length(maxes)+1:N %Make maxima where none can be found.
+    s=sort(peakv(1:k-1));
+    [trash,i]=max(s(2:end)-s(1:end-1)); %Find the biggest gap
+    i_=round((s(i+1)-s(i))/2); 
+    params(k).vM=v(:,i_);
+    params(k).pM=p(:,i_);
+    params(k).t0=t(s(i));
+    params(k).tf=t(s(i+1));
+    
+    X(1+6*(k-1):6*k)=[params(k).t0;params(k).tf;params(k).pM;params(k).vM];
+    peakv(k)=i_;
+end
+    
 
 
 
