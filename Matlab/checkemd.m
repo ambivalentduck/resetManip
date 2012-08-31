c742405d33fa9d4daaf6b73a5191ec5278faee8e
Justin Horowitz <justin.horowitz@gmail.com>
2012-08-31 16:28:46 -0500

    Updated checkemd to better compare hht vs my implementation

--- Matlab/checkemd.m
+++ Matlab/checkemd.m
@@ -2,18 +2,25 @@ clc
 clear all
 load ./Data/1extracted.mat
 
-for k=1:length(desiredTrajectories)
-    speed=sqrt(sum(desiredTrajectories(k).vDesired.^2,2))
+for k=1 %:5 %:length(desiredTrajectories)
+    speed=sqrt(sum(desiredTrajectories(k).vDesired.^2,2));
     figure(k)
-    subplot(2,1,1)
     clf
+    subplot(2,1,1)
     hold on
-    cycles=EMD(speed,3,0)
+    cycles=EMD(speed,3,0);
     t=desiredTrajectories(k).time;
     plot(t,speed,'b')
     plot(t,cycles(2).c,'r')
     plot(t,cycles(3).c,'k')
     
+    subplot(2,1,2)
+    hold on
+    hht=eemd(speed,.2,100);
+    plot(t,speed,'b')
+    for kk=2:size(hht,2)
+        plot(t,hht(:,kk),'r')
+    end
     
 end
     
