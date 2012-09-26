f8c0aef4b0bf58f7e9fc65f1a76c32475b309301
Justin Horowitz <justin.horowitz@gmail.com>
2012-09-26 12:41:22 -0500

    plotHumps now general

--- Matlab/plotHumps.m
+++ Matlab/plotHumps.m
@@ -1,36 +1,39 @@
-clc
-clear all
+function plotHumps(nums,prefix)
 
 close all
 
-for k=1:8
-    name=num2str(k);
-    load(['./Data/',name,'humps.mat']);
+for k=1:length(nums)
+    name=num2str(nums(k));
+    load(['./Data/',prefix,name,'humps.mat']);
 
     figure(1)
-    subplot(8,1,k)
+    subplot(length(nums),1,k)
     bar(plotme(2).errorBins,plotme(2).errorcounts)
     if k==1
         legend('All Humps','After EM','Before EM - Mode 2 Only','Residual')
         title('Peak-Peak Error Time Distance')
     end
     ylabel(name)
-    if k==8
+    if k==length(nums)
         xlabel('Time, milliseconds')
     end
-    suplabel('Relative Frequency','y')
 
     figure(2)
-    subplot(8,1,k)
+    subplot(length(nums),1,k)
     bar(plotme(2).timeBins,plotme(2).timecounts)
     if k==1
         title('Peak-Peak Period')
         legend('All Humps','After EM','Before EM - Mode 2 Only','Residual')
     end
     ylabel(name)
-    suplabel('Relative Frequency','y')
 
-    if k==8
+    if k==length(nums)
         xlabel('Time, milliseconds')
     end
 end
+
+figure(1)
+suplabel('Relative Frequency','y');
+
+figure(2)
+suplabel('Relative Frequency','y');
